c7c5e083617c0660e9cbce21038799fa5094890a
Stephen Visser <stephen.visser@devfacto.com>
2016-02-12 21:20:26 -0700

    Initial Commit

---
+++ EarthquakeTester/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EarthquakeTester
+//
+//  Created by Stephen Visser on 2016-02-12.
+//  Copyright © 2016 Svper. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
